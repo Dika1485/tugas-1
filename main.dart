f07e95de042f8cd4f62d847f12e6c3294facf6f2
Dika1485 <ptiandika@gmail.com>
2023-09-04 18:16:21 +0700

    push bro

--- main.dart
+++ main.dart
@@ -2,14 +2,18 @@ class Mahasiswa {
   String nim;
   String nama;
   int tahunLahir;
+  int usia=0;
 
-  Mahasiswa(this.nim, this.nama, this.tahunLahir);
+  Mahasiswa(this.nim, this.nama, this.tahunLahir){
+    this.usia=DateTime.now().year-tahunLahir;
+  }
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    print("Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun.");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021013", "Priandika Ratmadani Anugrah", 2004);
+  mhs.perkenalan();
 }
